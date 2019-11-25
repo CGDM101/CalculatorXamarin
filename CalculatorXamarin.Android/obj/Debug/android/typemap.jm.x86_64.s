350b2d1c7ab17a3c208f6c034b2e22703e989da6
Shahzad <searchpk@hotmail.com>
2019-11-25 14:44:19 +0100

    Initial commit

---
+++ CalculatorXamarin.Android/obj/Debug/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: b5a7fd3be04c1eb3924cd4da63d89cb95cdfd836 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1284
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 336409
+	.include	"typemap.jm.inc"
---
+++ CalculatorXamarin/SharedResources.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+
+namespace CalculatorXamarin
+{
+    public static class SharedResources
+    {
+        public static Color OpButtonBkColor
+        {
+            get
+            {
+                //return Color.White;
+                return Color.FromArgb(0xff, 0xa5, 0);
+
+            }
+        }
+    }
+}
